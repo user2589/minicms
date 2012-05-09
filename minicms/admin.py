de8985354c2af17bbc1263d8b3ae5e0f2330b540
Marat Valiev <valiev.m@gmail.com>
2012-05-10 02:06:17 +1100

    cleanup admin

--- minicms/admin.py
+++ minicms/admin.py
@@ -1,22 +1,11 @@
 # encoding: utf-8
 
 from django.contrib import admin
-from minicms.models import Page
-from django import forms
-from django_markdown.widgets import MarkdownWidget
+from minicms import models
+from django_markdown.admin import MarkdownModelAdmin
 
-
-class PageAdminForm(forms.ModelForm):
-    content = forms.CharField(widget=MarkdownWidget())
-
-    class Meta:
-        model = Page
-
-
-class PageAdmin(admin.ModelAdmin):
-    form = PageAdminForm
-
+class PageAdmin(MarkdownModelAdmin):
     list_display = ('name', 'lang', 'title')
     list_filter = ('lang',)
 
-admin.site.register(Page, PageAdmin)
+admin.site.register(models.Page, PageAdmin)
