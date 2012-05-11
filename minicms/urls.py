7da8be92d11b33e913408438fa174dc524f1d9d9
Pavel Puchkin <neoascetic@gmail.com>
2012-05-11 12:06:07 +1100

    Fixes in minicms.urls

--- minicms/urls.py
+++ minicms/urls.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 import views
-from django.conf.urls.defaults import *
+from django.conf.urls.defaults import url, patterns
 
 urlpatterns = patterns('',
-    url(r'^preview',    views.preview,    name='django_markdown_preview'),
-    (r'^(q?.+)/?$',     views.show_page),
+    url(r'^preview', views.preview, name='django_markdown_preview'),
+    (r'^(.+)/$', views.show_page),
 )
